7dcfa07e5ace0d2322e9e5787dbbdccba70abd96
Keane Dalisay <keane.dalisay0118@gmail.com>
2023-04-22 14:29:36 +0800

    Add footer cmpnt and styles

---
+++ pages/cmpnts/footer.php
@@ -0,0 +1,5 @@
+<footer class="ftr">
+  <small class="cprgt">Copyright © 2023. <a class="cprgt-link" href="https://wvsu.edu.ph/">West Visayas State
+      University</a>. All
+    rights reserved.</small>
+</footer>
