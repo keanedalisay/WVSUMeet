b5e2ec6f7a297511ff08a06f0a995765b0ed5c85
Keane Dalisay <keane.dalisay0118@gmail.com>
2023-04-26 13:48:37 +0800

    Add partial content of homepage and style it

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="application-name" content="Meet.WVSU">
+  <title>Meet.WVSU</title>
+  <link href="styles/global.css" rel="stylesheet">
+  <link href="styles/index.css" rel="stylesheet">
+</head>
+
+<body>
+  <?php require_once('pages/cmpnts/header.login.html') ?>
+  <main class="mn">
+    <section class="hdng">
+      <h1 class="hdng-meet">Meet</h1>
+      <h2 class="hdng-ppl">your classmates...</h2>
+      <a href="pages/signup.php" class="hdng-signup">Sign-Up</a>
+    </section>
+  </main>
+  <?php require_once('pages/cmpnts/footer.html') ?>
+
+</body>
+
+</html>
