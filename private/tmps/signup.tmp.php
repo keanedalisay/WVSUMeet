6fd7edf9e232f6b934abffc01eb0647ceeeb3c4d
Keane Dalisay <keane.dalisay0118@gmail.com>
2023-05-01 08:57:29 +0800

    Refactor signup page to use templates on processing

---
+++ private/tmps/signup.tmp.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="application-name" content="Meet.WVSU">
+  <title>Meet.WVSU</title>
+  <link href="../styles/global.css" rel="stylesheet">
+  <link href="../styles/action_form.css" rel="stylesheet">
+  <script src="../scripts/signup.js" defer></script>
+</head>
+
+<body>
+  <?php require_once('../../private/cmpnts/header.login.html') ?>
+  <main class="mn mn--signup">
+    <div class="wrap wrap--action_form">
+      <section class="hdng">
+        <h1 class="hdng-actn">Sign-Up</h1>
+        <p class="hdng-side">as a new user...</p>
+        <a class="hdng-link" href="login.php">or log-in if you already did.</a>
+      </section>
+      <section class="login">
+        <form class="form" action="signup.php" method="POST">
+
+          <label for="user_name" class="form-lbl">Name:</label>
+          <input id="user_name" type="text" name="user_name" class="form-input" />
+
+          <label for="user_wid" class="form-lbl">WVSU-ID:</label>
+          <input id="user_wid" type="text" name="user_wid" class="form-input" />
+
+          <label for="user_pswrd" class="form-lbl form-lbl--signup_pswrd">Password:</label>
+          <input id="user_pswrd" type="password" name="user_pswrd" class="form-input" />
+
+          <label for="user_cnfrm_pswrd" class="form-lbl">Confirm:</label>
+          <input id="user_cnfrm_pswrd" type="password" name="user_cnfrm_pswrd" class="form-input" />
+
+          <button type="submit" class="form-submit">Sign-Up</button>
+        </form>
+      </section>
+    </div>
+  </main>
+  <?php require_once('../../private/cmpnts/footer.html') ?>
+</body>
+
+</html>
--- public/pages/signup.php
+++ public/pages/signup.php
@@ -1,41 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  session_name('CREDS');
+  session_start();
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="application-name" content="Meet.WVSU">
-  <title>Meet.WVSU</title>
-  <link href="../styles/global.css" rel="stylesheet">
-  <link href="../styles/action_form.css" rel="stylesheet">
-  <script src="../scripts/signup.js" defer></script>
-</head>
+  $conn = mysqli_connect('localhost', 'root', '', 'meet.wvsu');
 
-<body>
-  <?php require_once('../../private/cmpnts/header.login.html') ?>
-  <main class="mn mn--signup">
-    <div class="wrap wrap--action_form">
-      <section class="hdng">
-        <h1 class="hdng-actn">Sign-Up</h1>
-        <p class="hdng-side">as a new user...</p>
-        <a class="hdng-link" href="login.php">or log-in if you already did.</a>
-      </section>
-      <section class="login">
-        <form class="form" action="../../private/process/signup.php" method="POST">
-          <label for="user_name" class="form-lbl">Name:</label>
-          <input id="user_name" type="text" name="user_name" class="form-input" />
-          <label for="user_wid" class="form-lbl">WVSU-ID:</label>
-          <input id="user_wid" type="text" name="user_wid" class="form-input" />
-          <label for="user_pswrd" class="form-lbl form-lbl--signup_pswrd">Password:</label>
-          <input id="user_pswrd" type="password" name="user_pswrd" class="form-input" />
-          <label for="user_cnfrm_pswrd" class="form-lbl">Confirm:</label>
-          <input id="user_cnfrm_pswrd" type="password" name="user_cnfrm_pswrd" class="form-input" />
-          <button type="submit" class="form-submit">Sign-Up</button>
-        </form>
-      </section>
-    </div>
-  </main>
-  <?php require_once('../../private/cmpnts/footer.html') ?>
-</body>
+  $user_name = htmlspecialchars($_POST['user_name']);
+  $user_pswrd = password_hash(htmlspecialchars($_POST['user_pswrd']), PASSWORD_BCRYPT);
+  $user_wid = htmlspecialchars($_POST['user_wid']);
 
-</html>
+  $user_sql = "INSERT INTO users(Name, Password, WVSU_ID) VALUES(?, ?, ?)";
+  $prep_sql = mysqli_prepare($conn, $user_sql);
+  mysqli_stmt_bind_param($prep_sql, 'sss', $user_name, $user_pswrd, $user_wid);
+  mysqli_stmt_execute($prep_sql);
+  mysqli_stmt_close($prep_sql);
+
+  $_SESSION['user_name'] = $user_name;
+  $_SESSION['user_wid'] = $user_wid;
+  header('Location: dshbrd.php');
+}
+
+require_once('../../private/tmps/signup.tmp.php');
+?>
