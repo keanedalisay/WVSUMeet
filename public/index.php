2b4a9c57b83759095f245a7fcfe246ec4162e7cb
Keane Dalisay <keane.dalisay0118@gmail.com>
2023-05-01 09:09:37 +0800

    Refactor home page to use templates on processing

--- public/index.php
+++ public/index.php
@@ -1,28 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+session_name('CREDS');
+session_start();
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="application-name" content="Meet.WVSU">
-  <title>Meet.WVSU</title>
-  <link href="styles/global.css" rel="stylesheet">
-  <link href="styles/index.css" rel="stylesheet">
-  <script src="scripts/index.js" defer></script>
-</head>
-
-<body>
-  <?php require_once('../private/cmpnts/header.login.html') ?>
-  <main class="mn">
-    <section class="hdng">
-      <h1 class="hdng-meet">Meet</h1>
-      <h2 class="hdng-ppl">your classmates...</h2>
-      <a href="pages/signup.php" class="hdng-signup">Sign-Up</a>
-    </section>
-    <section class="ptrts" data-slctr="ptrts">
-    </section>
-  </main>
-  <?php require_once('../private/cmpnts/footer.html') ?>
-</body>
-
-</html>
+require_once('../private/tmps/index.tmp.php');
+?>
