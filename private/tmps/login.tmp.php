1f89c1ac6d710e6f014e57d783e94bc5fd1f6701
Keane Dalisay <keane.dalisay0118@gmail.com>
2023-05-01 09:48:18 +0800

    Add func. to login as user and show dashboard

---
+++ private/tmps/login.tmp.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="application-name" content="Meet.WVSU">
+  <title>Meet.WVSU</title>
+  <link href="../styles/global.css" rel="stylesheet">
+  <link href="../styles/action_form.css" rel="stylesheet">
+  <script src="../scripts/login.js" defer></script>
+</head>
+
+<body>
+  <?php require_once('../../private/cmpnts/header.login.php') ?>
+  <main class="mn">
+    <div class="wrap wrap--action_form">
+      <section class="hdng">
+        <h1 class="hdng-actn">Log-in</h1>
+        <p class="hdng-side">to your account...</p>
+        <a class="hdng-link" href="signup.php">or sign-up for a new one</a>
+      </section>
+      <section class="login">
+        <form class="form" action="login.php" method="POST">
+
+          <label for="user_wid" class="form-lbl">WVSU-ID:</label>
+          <input id="user_wid" type="text" name="user_wid" class="form-input" />
+
+          <label for="user_pswrd" class="form-lbl">Password:</label>
+          <input id="user_pswrd" type="password" name="user_pswrd" class="form-input" />
+
+          <button type="submit" class="form-submit">Log-in</button>
+        </form>
+      </section>
+    </div>
+  </main>
+  <?php require_once('../../private/cmpnts/footer.html') ?>
+</body>
+
+</html>
--- public/pages/login.php
+++ public/pages/login.php
@@ -1,37 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  session_name('CREDS');
+  session_start();
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="application-name" content="Meet.WVSU">
-  <title>Meet.WVSU</title>
-  <link href="../styles/global.css" rel="stylesheet">
-  <link href="../styles/action_form.css" rel="stylesheet">
-  <script src="../scripts/login.js" defer></script>
-</head>
+  $user_wid = htmlspecialchars($_POST['user_wid']);
+  $user_pswrd = htmlspecialchars($_POST['user_pswrd']);
 
-<body>
-  <?php require_once('../../private/cmpnts/header.login.html') ?>
-  <main class="mn">
-    <div class="wrap wrap--action_form">
-      <section class="hdng">
-        <h1 class="hdng-actn">Log-in</h1>
-        <p class="hdng-side">to your account...</p>
-        <a class="hdng-link" href="signup.php">or sign-up for a new one</a>
-      </section>
-      <section class="login">
-        <form class="form" action="../../private/process/login.php" method="POST">
-          <label for="user_wid" class="form-lbl">WVSU-ID:</label>
-          <input id="user_wid" type="text" name="user_wid" class="form-input" />
-          <label for="user_pswrd" class="form-lbl">Password:</label>
-          <input id="user_pswrd" type="password" name="user_pswrd" class="form-input" />
-          <button type="submit" class="form-submit">Log-in</button>
-        </form>
-      </section>
-    </div>
-  </main>
-  <?php require_once('../../private/cmpnts/footer.html') ?>
-</body>
+  $conn = mysqli_connect('localhost', 'root', '', 'meet.wvsu');
 
-</html>
+  $user_sql = "SELECT * FROM users WHERE WVSU_ID = '$user_wid'";
+  $user_query = mysqli_query($conn, $user_sql);
+  $user = mysqli_fetch_assoc($user_query);
+
+  $_SESSION['user_name'] = $user['Name'];
+  $_SESSION['user_wid'] = $user['WVSU_ID'];
+
+  $pswrd_is_match = password_verify($user_pswrd, $user['Password']);
+  if ($pswrd_is_match) {
+    header('Location: dshbrd.php');
+    exit;
+  }
+}
+
+require_once('../../private/tmps/login.tmp.php');
+?>
