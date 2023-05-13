b1092c736fb6fe671f03d623a7e1cc8f2ea1e744
Keane Dalisay <keane.dalisay0118@gmail.com>
2023-05-13 10:56:41 +0800

    Refactor styles of dshbrd for pre-2020 compatability

--- private/tmps/dshbrd.tmp.php
+++ private/tmps/dshbrd.tmp.php
@@ -14,26 +14,28 @@
 <body>
   <?php require_once('../../private/cmpnts/header.logout.php') ?>
   <main class="mn">
-    <section class="hdng">
-      <h1 class="hdng-user">Welcome
-        <?php echo $user_name[0] ?>
-      </h1>
-      <hr class="hdng-hr">
-    </section>
-    <section class="msgbox">
-      <div class="wrap wrap--msgs">
-        <ol class="msgs" data-slctr="msgbox" tabindex="0" aria-label="Message box">
-          <?php require_once('../../private/msg_system/show_gbl_msgs.php') ?>
-        </ol>
-      </div>
-      <form class="wdgt" action="../../private/msg_system/prcss_gbl_msg.php" method="POST">
-        <textarea class="wdgt-input" name="user_gbl_msg" placeholder="Enter your message..."
-          aria-label="Global message input"></textarea>
-        <button class="wdgt-sbmt" type="submit" aria-label="Send message">
-          <img src="../icons/send_msg_icon.svg" alt="Send message">
-        </button>
-      </form>
-    </section>
+    <div class="wrap wrap--dshbrd">
+      <section class="hdng">
+        <h1 class="hdng-user">Welcome
+          <?php echo $user_name[0] ?>
+        </h1>
+        <hr class="hdng-hr">
+      </section>
+      <section class="msgbox">
+        <div class="wrap wrap--msgs">
+          <ol class="msgs" data-slctr="msgbox" tabindex="0" aria-label="Message box">
+            <?php require_once('../../private/msg_system/show_gbl_msgs.php') ?>
+          </ol>
+        </div>
+        <form class="wdgt" action="../../private/msg_system/prcss_gbl_msg.php" method="POST">
+          <textarea class="wdgt-input" name="user_gbl_msg" placeholder="Enter your message..."
+            aria-label="Global message input"></textarea>
+          <button class="wdgt-sbmt" type="submit" aria-label="Send message">
+            <img src="../icons/send_msg_icon.svg" alt="Send message">
+          </button>
+        </form>
+      </section>
+    </div>
   </main>
   <?php require_once('../../private/cmpnts/footer.html') ?>
 </body>
