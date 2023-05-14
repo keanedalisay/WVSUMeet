20fb4fc550f762d9034c8b390ebf98da4da9f835
Keane Dalisay <keane.dalisay0118@gmail.com>
2023-05-14 14:24:06 +0800

    Refactor logic of login, add id and pswrd validation

--- private/tmps/login.tmp.php
+++ private/tmps/login.tmp.php
@@ -22,12 +22,24 @@
       </section>
       <section class="login">
         <form class="form" action="login.php" method="POST">
-
           <label for="user_wid" class="form-lbl">WVSU-ID:</label>
-          <input id="user_wid" type="text" name="user_wid" class="form-input" />
+          <input id="user_wid" type="text" name="user_wid" value="<?php echo $log_in->getInputWid() ?>"
+            class="form-input" aria-errormessage="user_err_wid"
+            aria-invalid="<?php echo $log_in->setAriaInvalid('err_wid') ?>" required />
+          <p id="user_err_wid" class="form-err_lbl <?php echo $log_in->setErrClass('err_wid') ?>" aria-live="assertive">
+            <?php echo $log_in->getErrWid() ?>
+          </p>
 
           <label for="user_pswrd" class="form-lbl">Password:</label>
-          <input id="user_pswrd" type="password" name="user_pswrd" class="form-input" />
+          <input id="user_pswrd" type="password" name="user_pswrd" value="<?php echo $log_in->getInputPswrd() ?>"
+            class="form-input" aria-errormessage="user_err_pswrd"
+            aria-invalid="<?php echo $log_in->setAriaInvalid('err_pswrd') ?>" required />
+          <p id="user_err_pswrd" class="form-err_lbl <?php echo $log_in->setErrClass('err_pswrd') ?>"
+            aria-live="assertive">
+            <?php echo $log_in->getErrPswrd() ?>
+          </p>
+
+          <input id="tkn_login" type="hidden" name="tkn_login" value=<?php echo $log_in->getToken() ?> />
 
           <button type="submit" class="form-submit">Log-in</button>
         </form>
