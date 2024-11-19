cd7cd7b0686df5d81b9e2559912f1a89b0b90b75
keanedalisay <keandals21@gmail.com>
2024-11-19 11:35:46 +0800

    Add PHP Router libary

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+require_once dirname(__DIR__, 1)."/vendor/autoload.php";
+require_once __DIR__.'/router.php';
+
+session_name("WVSUMeet");
+
+get("/", "pages/index.php");
+get("/sign-up", "pages/sign-up.php");
+get("/log-in", "pages/log-in.php");
+get("/chat", "pages/chat.php");
+
+post("/log-in", "pages/log-in.php");
