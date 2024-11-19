8626f0b2ff64c5929b7a593a96612577a57c2ae3
keanedalisay <keandals21@gmail.com>
2024-11-19 11:33:44 +0800

    Add temporary home page

---
+++ public/pages/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>WVSUMeet</title>
+</head>
+<body>
+  <h1>Welcome to WVSUMeet!</h1>
+  <a href="/log-in">Log-in Account</a>
+  <a href="/sign-up">Create a New Account</a>
+</body>
+</html>
