a1e98e05d3b333419698b5df2c63f7be4cdf87ba
keanedalisay <keandals21@gmail.com>
2024-11-27 14:11:28 +0800

    Add ability to preview profile image

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -3,12 +3,34 @@ const formUserProfile = document.querySelector("[data-js=form-user-profile]");
 const buttonSaveUserProfile = document.querySelector("[data-js=button-user-profile-save]");
 const buttonCancelUserProfile = document.querySelector("[data-js=button-user-profile-cancel]");
 
-inputUserProfile.addEventListener("input", (e) => {
+const profileImg = document.querySelector("[data-js=user-profile]");
+let userProfile = profileImg.getAttribute("src") || "";
+
+inputUserProfile.addEventListener("input", () => {
   buttonSaveUserProfile.parentElement.classList.remove("form__controls--hide");
+
+  const form = new FormData(formUserProfile);
+  const file = form.get("user_profile_img");
+
+  const profileNoImg = document.querySelector("[data-js=user-default-profile]");
+
+  profileImg.setAttribute("src", URL.createObjectURL(file));
+  profileImg.setAttribute("alt", file.name);
+
+  profileNoImg.classList.add("user-profile--hide");
+  profileImg.classList.remove("user-profile--hide");
 });
 
 buttonCancelUserProfile.addEventListener("click", (e) => {
   buttonSaveUserProfile.parentElement.classList.add("form__controls--hide");
+  const profileNoImg = document.querySelector("[data-js=user-default-profile]");
+  
+  if (!profileImg.classList.contains("user-profile--hide")) {
+    profileNoImg.classList.remove("user-profile--hide");
+    profileImg.classList.add("user-profile--hide");
+  }
+  else profileImg.setAttribute("src", userProfile);
+
 });
 
 const inputUserName = document.querySelector("[data-js=input-user-name]");
@@ -21,21 +43,4 @@ inputUserName.addEventListener("input", (e) => {
 
 buttonCancelUserDetails.addEventListener("click", (e) => {
   buttonSaveUserDetails.parentElement.classList.add("form__controls--hide");
-});
-
-formUserProfile.addEventListener("submit", (e) => {
-  e.preventDefault();
-  buttonSaveUserProfile.parentElement.classList.add("form__controls--hide");
-  const form = new FormData(e.target);
-  const file = form.get("user_profile_img");
-
-  const profileImg = document.querySelector("[data-js=user-profile]");
-  const profileNoImg = document.querySelector("[data-js=user-default-profile]");
-
-  profileImg.setAttribute("src", URL.createObjectURL(file));
-  profileImg.setAttribute("alt", file.name);
-
-  profileNoImg.classList.add("user-profile--hide");
-  profileImg.classList.remove("user-profile--hide");
-  e.target.submit();
 });
