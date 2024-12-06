0918317207a8ef3c8443d33ebdb8e7965e6e2783
keanedalisay <keandals21@gmail.com>
2024-12-06 13:47:39 +0800

    Add styles and markup to the index page

--- public/pages/index.php
+++ public/pages/index.php
@@ -13,10 +13,15 @@ if (isset($_SESSION["has_logged_in"]) && $_SESSION["has_logged_in"] === true) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>WVSUMeet</title>
+  <link href="../styles/global.css" rel="stylesheet" />
+  <link href="../styles/index.css" rel="stylesheet" />
 </head>
 <body>
-  <h1>Welcome to WVSUMeet!</h1>
-  <a href="/log-in">Log-in Account</a>
-  <a href="/sign-up">Create a New Account</a>
+  <img src="../assets/images/wvsumeet.png" class="logo" />
+  <p>A real-time chat application catered to West Visayas State University (WVSU) students. </p>
+  <div class="links">
+    <a href="/log-in">Log-in to Account</a>
+    <a href="/sign-up">Create a New Account</a>
+  </div>
 </body>
 </html>
